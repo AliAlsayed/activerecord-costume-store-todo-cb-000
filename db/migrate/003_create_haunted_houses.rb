836507eef2a545270eeee0005756535772478855
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-01 09:05:37 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.integer :num_of_employees
       t.boolean :family_friendly
       t.datetime :opening_time
       t.datetime :closing_time
