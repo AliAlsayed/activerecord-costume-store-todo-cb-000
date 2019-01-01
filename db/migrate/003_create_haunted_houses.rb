3e82f6c6789269f98a39fa389b84ce834c96ebaf
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-01 09:04:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,12 +1,14 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.float :price
-      t.string :image_url
-      t.string :size
-      t.timestamps
+      t.integer :costume_inventory
+      t.string :location
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
