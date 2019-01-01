0307a0cae3a05d36156c7106fa8b2ce46aff12ca
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-01 09:22:47 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-Create your haunted_houses migration here
+#Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
