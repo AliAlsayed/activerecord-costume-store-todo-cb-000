7892d6d5467e59b6a296aa010bb09b5a47f96f7e
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-01 09:05:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.integer :costume_inventory
       t.string :location
       t.integer :num_of_employees
       t.boolean :family_friendly
