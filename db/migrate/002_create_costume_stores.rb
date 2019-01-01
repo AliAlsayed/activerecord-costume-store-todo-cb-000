e2708e228cd693e027bfcc46a36e8896658356d3
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-01 08:55:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
